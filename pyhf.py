29fea13a2c7bc2262e73f4f58b2684df6f667eb7
Fülöp Bulcsú <fulopbulcsu78@gmail.com>
2023-02-25 11:37:36 +0100

    Felvesunk ketto py fajlt

---
+++ pyhf.py
@@ -0,0 +1,15 @@
+string = 'Palfi Aranka I'
+string2 = 'Gál Asztrik I'
+string3 = 'Fokdes Nora N'
+if string[-1] == 'I':
+    print('Igen')
+else: 
+    print('NEm')
+if string2[-1] == 'I':
+    print('Igen')
+else: 
+    print('NEm')
+if string3[-1] == 'I':
+    print('Igen')
+else: 
+    print('NEm')
